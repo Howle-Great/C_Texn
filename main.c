973b6b1b6e8e6debcacf34c081f6fa33d30b1a35
Howle <mdv413@mail.ru>
2018-10-08 14:58:26 +0300

    Проходит все тесты, но есть записи от Xcode

--- main.c
+++ main.c
@@ -9,10 +9,12 @@
 char** StrFilt(char** str, int num_lines, int* num_of_correct_strs){
     char **out_of_str = (char **)malloc(num_lines*sizeof(char*));
     *num_of_correct_strs = 0;
+    int counter_simb = 0;
     for (int y = 0; y < num_lines; y++) {
         int count = 0;
         if (strcmp(str[y], "\n")) {
-            for (int i = 0; i < strlen(*(str + y)); i++) {
+            int i = 0;
+            for (; i < strlen(*(str + y)); i++) {
                 
                 if (str[y][i] == '(') {
                     count++;
@@ -20,20 +22,23 @@ char** StrFilt(char** str, int num_lines, int* num_of_correct_strs){
                 if (str[y][i] == ')') {
                     count--;
                 }
-                if (count < 0) {
+                if (count < 0) { 
                     break;
                 }
             }
+            counter_simb += i;
             if (count == 0) {
-                memcpy(&out_of_str[(*num_of_correct_strs)], &(*(str + y)), sizeof(*(str + y))+1);
+                memcpy(&out_of_str[(*num_of_correct_strs)], &(str[y]), sizeof(str[y]));
+                //strcpy(&(str[y]), &out_of_str[*num_of_correct_strs]);
                 (*num_of_correct_strs)++;
             }
         }
     }
+    out_of_str = (char**) realloc(out_of_str, *num_of_correct_strs*counter_simb*sizeof(char));
     return out_of_str;
 }
 int main(int argc, const char * argv[]) {
-    int len_of_str = 1;
+    int len_of_str = 2;
     int num_of_strs = 1;
     int log_num_of_strs = 0;
     char** strs =  (char**) malloc(num_of_strs*sizeof(char*));
